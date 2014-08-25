686de9604fe73fbe85d1f7b7e9a092fca1658d36
James McKay <code@jamesmckay.net>
2014-08-25 11:24:03 +0100

    Implement sorting for posts query.

---
+++ src/JammyCakes.Blog.Core/Queries/PostSortOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JammyCakes.Blog.Core.Queries
+{
+    public enum PostSortOrder
+    {
+        Date,
+        Title
+    }
+}
--- src/JammyCakes.Blog.Core/Queries/PostsQuery.cs
+++ src/JammyCakes.Blog.Core/Queries/PostsQuery.cs
@@ -10,8 +10,26 @@ namespace JammyCakes.Blog.Core.Queries
 {
     public class PostsQuery : PagingQuery<Post>
     {
-        public IQueryable<Post> GetPosts(IDbSet<Post> source)
+        public PostSortOrder Order { get; set; }
+
+        public bool Descending { get; set; }
+
+        public IQueryable<Post> GetPosts(IQueryable<Post> source)
         {
+            switch (this.Order)
+            {
+                case PostSortOrder.Date:
+                    source = Descending
+                        ? source.OrderByDescending(x => x.PublicationDate)
+                        : source.OrderBy(x => x.PublicationDate);
+                    break;
+                case PostSortOrder.Title:
+                    source = Descending
+                        ? source.OrderByDescending(x => x.Title)
+                        : source.OrderBy(x => x.Title);
+                    break;
+            }
+
             return base.ApplyPaging(source);
         }
     }
